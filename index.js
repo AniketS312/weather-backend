00eb6ddb83cab8d06f850727cb336d01d1008bbd
AniketS312 <58675185+AniketS312@users.noreply.github.com>
2025-03-30 20:46:23 -0600

    adding cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const https = require('node:https');
 const app = express()
+const cors = require('cors')
 
 const port = process.env.PORT || 5000
 
@@ -9,7 +10,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/:q', async (req, res) => {
+var whitelist = ['http://localhost:5173', 'http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.get('/:q', cors(corsOptions), async (req, res) => {
     const q = req.params.q
     const appid = process.env.WEATHER_API_KEY
     const url = `${process.env.WEATHER_APP_URI}?${q}&appid=${appid}`
