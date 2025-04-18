e3c766092148c117c635550c2096dd561e596c38
AniketS312 <aniketsharma312@gmail.com>
2025-04-18 12:52:33 -0600

    final review of get requests

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const cors = require('cors')
 
 const port = process.env.PORT || 5000
 
+// Cancel out the automatic favicon search done by browser
 app.get('/favicon.ico', (req, res) => {
   res.status(204).end(); // Respond with No Content
 });
 
+// For testing purposes only - No need to remove
 app.get('/',cors(corsOptions), (req, res) => {
   res.send('Hello World!')
 })
 
 
-
+// Featch both weather and image data from the APIs
 app.get('/:q', cors(corsOptions), async (req, res) => {
     const q = req.params.q
     // Weather data fetching
@@ -26,7 +28,6 @@ app.get('/:q', cors(corsOptions), async (req, res) => {
     const imageUrl = `${process.env.UNSPLASH_APP_URI}?client_id=${process.env.UNSPLASH_API_KEY}&query=${q}&orientation=landscape&per_page=5&page=1`
     const imageResponse = await fetch(imageUrl)
     const imageData = await imageResponse.json()
-    console.log(imageUrl)
     res.send({
       weather: weatherData,
       image: imageData
@@ -34,7 +35,27 @@ app.get('/:q', cors(corsOptions), async (req, res) => {
 
 });
 
-var whitelist = ['http://localhost:5173', 'http://localhost:3000']
+// duplicate of the above for testing purposes
+app.get('search/:q', cors(corsOptions), async (req, res) => {
+  const q = req.params.q
+  // Weather data fetching
+  const weatherUrl = `${process.env.WEATHER_APP_URI}?q=${q}&appid=${process.env.WEATHER_API_KEY}`
+  const weatherResponse = await fetch(weatherUrl) 
+  const weatherData = await weatherResponse.json() 
+  // Image data fetching
+  const imageUrl = `${process.env.UNSPLASH_APP_URI}?client_id=${process.env.UNSPLASH_API_KEY}&query=${q}&orientation=landscape&per_page=5&page=1`
+  const imageResponse = await fetch(imageUrl)
+  const imageData = await imageResponse.json()
+  console.log(imageUrl)
+  res.send({
+    weather: weatherData,
+    image: imageData
+  })
+
+});
+
+// CORS setup 
+var whitelist = ['https://weather-backend-6kka.onrender.com']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -44,6 +65,18 @@ var corsOptions = {
     }
   }
 }
+
+// -------For testing purposes only - No need to remove. Works with URl searchs
+// var whitelist = ['http://localhost:5173', 'http://localhost:3000']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
