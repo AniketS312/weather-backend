826091bb30430dde995e828a2653bf85290fb472
AniketS312 <aniketsharma312@gmail.com>
2025-04-18 12:10:31 -0600

    added working fetch for both image and weather data

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ const cors = require('cors')
 
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
+app.get('/favicon.ico', (req, res) => {
+  res.status(204).end(); // Respond with No Content
+});
+
+app.get('/',cors(corsOptions), (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+app.get('/:q', cors(corsOptions), async (req, res) => {
+    const q = req.params.q
+    // Weather data fetching
+    const weatherUrl = `${process.env.WEATHER_APP_URI}?q=${q}&appid=${process.env.WEATHER_API_KEY}`
+    const weatherResponse = await fetch(weatherUrl) 
+    const weatherData = await weatherResponse.json() 
+    // Image data fetching
+    const imageUrl = `${process.env.UNSPLASH_APP_URI}?client_id=${process.env.UNSPLASH_API_KEY}&query=${q}&orientation=landscape&per_page=5&page=1`
+    const imageResponse = await fetch(imageUrl)
+    const imageData = await imageResponse.json()
+    console.log(imageUrl)
+    res.send({
+      weather: weatherData,
+      image: imageData
+    })
+
+});
 
 var whitelist = ['http://localhost:5173', 'http://localhost:3000']
 var corsOptions = {
@@ -20,16 +44,6 @@ var corsOptions = {
     }
   }
 }
-
-app.get('/:q', cors(corsOptions), async (req, res) => {
-    const q = req.params.q
-    const appid = process.env.WEATHER_API_KEY
-    const url = `${process.env.WEATHER_APP_URI}?${q}&appid=${appid}`
-    console.log(q, appid)
-    const response = await fetch(url) // Await the fetch call
-    const data = await response.json() // Parse the JSON response
-    res.send(data) 
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
